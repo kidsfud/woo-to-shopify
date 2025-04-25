4ffe632ef7d88426ca960c70509891e1a9ad7b6d
Teja <kidsfud2@gmail.com>
2025-04-25 18:31:53 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // /// -----------------------------------------------------------------------------------
 
-require("dotenv").config();
+// require("dotenv").config();
 const express    = require("express");
 const bodyParser = require("body-parser");
 const axios      = require("axios");
